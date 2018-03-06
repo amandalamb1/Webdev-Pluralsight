4156b9426883ecf555339c24d52316a3af76afa1
Amanda Lamb <amandalamb@Amandas-MacBook-Pro.local>
2018-03-06 16:16:55 -0700

    Halway throuhg jQuery tutuorial

--- js/index.js
+++ js/index.js
@@ -1,13 +1,93 @@
 //Index.js//
+
+$(document).ready(function(){
+
+
 "use strict";
 
-var msg = "hello Javascript";
+
+
+/* var msg = "hello Javascript";
 console.log(msg);
 
 var resultsdiv = document.getElementById("results");
-resultsdiv.innerHTML = "<p>This is from Javascript</p>"
+resultsdiv.innerHTML = "<p>This is from Javascript</p>";*/
+ 
+var resultList = $("#resultList");
+resultList.text("This is from JQuery");
 
-console.log("msg is " +typeof(msg));
+var toggleButton = $("#toggleButton");
+toggleButton.on("click", function () {
+    resultList.toggle(500);
+
+    if (toggleButton.text()== "Hide") toggleButton.text("Show");
+    else toggleButton.text ("Hide");
+
+});
+
+var listItems = $("header nav li");
+listItems.css("font-weight", "bold");
+listItems.css("font-size", "18px");
+
+
+
+
+
+
+/*
+var result = {
+    name: "jQuery",
+    language: "Javascript",
+    score: 4.5,
+    showlog: function (){
+
+    },
+    owner: {
+        login: "amandalamb",
+        id: 123456
+    }
+};
+
+result.phoneNumber = "123-456-7890";
+
+console.log(result.phoneNumber);
+
+var results = [{
+    name: "jQuery",
+    language: "Javascript",
+    score: 4.5,
+    showlog: function (){
+
+    },
+    owner: {
+        login: "amandalamb",
+        id: 123456
+    }
+},{
+    name: "jQuery UI",
+    language: "Javascript",
+    score: 3.5,
+    showlog: function (){
+
+    },
+    owner: {
+    login: "amandalamb",
+    id: 123456
+    }
+}];
+
+results.push(result);
+results.push({
+    name: "dummy project"
+});
+
+for (var x = 0; x < results.length; x++) {
+    var result = results[x];
+    if (result.score > 4) continue;
+    console.log(result.name);
+}
+ */
+/* console.log("msg is " +typeof(msg));
 console.log("resultsDiv is " +typeof(resultsDiv));
 
 var none;
@@ -63,6 +143,13 @@ function testMe() {
 
     var someMsg = "some Message";
     console.log("testme(): " + someMsg); 
+
+    showItThen("with Closure", function () {
+        showIt("testMe with closure(): " + someMsg);
+    });
+    
 }
 
-testMe();
+testMe(); */
+
+});
