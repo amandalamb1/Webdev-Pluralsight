d9c7b6678d723af09bdbdc58e192c4bc145bcc55
Amanda Lamb <amandalamb@Amandas-MacBook-Pro.local>
2018-03-29 11:01:59 -0600

    web dev tutorial done

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ console.log(msg);
 
 var resultsdiv = document.getElementById("results");
 resultsdiv.innerHTML = "<p>This is from Javascript</p>";*/
- 
+
 var resultList = $("#resultList");
 resultList.text("This is from JQuery");
 
@@ -20,14 +20,97 @@ var toggleButton = $("#toggleButton");
 toggleButton.on("click", function () {
     resultList.toggle(500);
 
-    if (toggleButton.text()== "Hide") toggleButton.text("Show");
+    if (toggleButton.text() == "Hide") toggleButton.text("Show");
     else toggleButton.text ("Hide");
 
 });
 
 var listItems = $("header nav li");
 listItems.css("font-weight", "bold");
-listItems.css("font-size", "18px");
+listItems.filter(":first").css("font-size", "18px");
+
+$("#gitHubSearchForm").on("submit", function (){
+
+    var searchPhrase = $("#SearchPhrase").val();
+    var useStars = $("#useStars").val();
+    var langChoice = $("#langChoice").val();
+
+
+    if (searchPhrase) {
+
+        resultList.text("Performing Search. . .");
+
+        var gitHubSearch = "https://api.github.com/search/repositories?q=" + encodeURIComponent(searchPhrase);
+
+        if (langChoice != "All") {
+            gitHubSearch += "+language:" + encodeURIComponent(langChoice);
+        }
+
+        if (useStars) {
+            gitHubSearch += "&sort=stars";
+        }      
+       // var gitHubSearch = "https://api.gitub.com/search/repositories?q=jquery+language:javascript&sort=stars"
+
+        $.get(gitHubSearch)
+            .success(function(r) {
+             //console.log(r.items.length);
+                displayResults(r.items);
+            })
+            .fail(function(err) {
+                console.log("Failed to query Github");
+            })
+            .done(function(){
+                    //
+            });
+    }
+    return false;
+});
+
+
+// var results = [{
+//     name: "jQuery",
+//     language: "Javascript",
+//     score: 4.5,
+//     showlog: function (){
+//     },
+//     owner: {
+//         login: "amandalamb",
+//         id: 123456
+//     }
+// }, {
+//     name: "jQuery UI",
+//     language: "Javascript",
+//     score: 3.5,
+//     showlog: function (){
+
+//     },
+//     owner: {
+//     login: "amandalamb",
+//     id: 123456
+//     }
+// }];
+
+function displayResults(results) {
+    resultList.empty();
+    $.each(results, function (i, item){
+        var newResult = $("<div class='result'>" + "<div class = 'title'>" + item.name + "</div>"
+        +"<div>Language: " + item.language + "</div>" +"<div>Owner: " +item.owner.login + "</div>" + "</div>");
+
+        newResult.hover(function(){
+            //make it dark
+            $(this).css("background-color", "lightgray");
+        }, function () {
+            // reverse
+            $(this).css("background-color", "transparent");
+        });
+
+        resultList.append(newResult);
+
+    });
+}
+
+
+
 
 
 
